4ec982784bb64c471c36adc790bd456b3ac14f35
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 11:02:21 -0300

    Create media-player.component.ts

---
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-media-player',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './media-player.component.html',
+  styleUrl: './media-player.component.css'
+})
+export class MediaPlayerComponent {
+
+}
