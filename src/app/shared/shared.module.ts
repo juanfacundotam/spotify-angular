66eb072dbd606be6aa4c8cc6ef4845db564c33af
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 16:30:27 -0300

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideBarComponent,
+    // Otros componentes que puedas tener en SharedModule
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    // Otros módulos necesarios para SharedModule
+  ],
+  exports: [
+    SideBarComponent,
+    // Otros componentes que quieras exportar
   ]
 })
 export class SharedModule { }
