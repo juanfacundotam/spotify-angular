156552f163aea01e808798d644078c75d8577291
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 18:04:07 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
