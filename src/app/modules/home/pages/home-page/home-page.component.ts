e88e4232a9d0e70f60d20f297d427e6854445e5f
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 18:04:15 -0300

    Update home-page.component.ts

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SideBarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
