19746c31f39ced0d04056c91dd9b11a42940af9e
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 18:04:11 -0300

    Update home.module.ts

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { SharedModule } from '@shared/shared.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
@@ -11,6 +11,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
+    SideBarComponent
   ]
 })
 export class HomeModule { }
