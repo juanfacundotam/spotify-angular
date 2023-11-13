55e22ea0643b396ae0cd3af890fd8b76914af5a2
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 10:01:53 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,5 @@ import { Routes } from '@angular/router';
 import { ExampleComponent } from './example/example.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component:ExampleComponent
-    },
-    {
-        path: 'hollo',
-        component:ExampleComponent
-    },
 
 ];
