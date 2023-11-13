074370d5c417fa2450684cc118555b3e726484c4
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 11:01:53 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { ExampleComponent } from './example/example.component';
 
 export const routes: Routes = [
-
+{
+    path: 'home',
+    loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule) 
+}
 ];
