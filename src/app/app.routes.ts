1b68a802763ed9faeb0cf623c639248eff3ed893
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 09:59:41 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component:ExampleComponent
+    },
+    {
+        path: 'hollo',
+        component:ExampleComponent
+    },
+
+];
