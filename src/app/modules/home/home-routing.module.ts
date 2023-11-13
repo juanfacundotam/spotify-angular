39fc502cc0ccf7df45de1da096dcbdf883f174f7
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 11:01:55 -0300

    Update home-routing.module.ts

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+// import { SideBarComponent } from '../../shared/components/side-bar/side-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: SideBarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
