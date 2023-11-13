af3024f36d79450aceb1815584bd5c52ef978b49
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-13 16:30:14 -0300

    Update home.module.ts

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-
+import { SharedModule } from '@shared/shared.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
+    HomePageComponent,
+    // Otros componentes que puedas tener en HomeModule
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule, // Importa SharedModule aquí
   ]
 })
 export class HomeModule { }
