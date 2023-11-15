3ec501c414bc27acaf5e800513d784a38828b42c
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 11:13:20 -0300

    Create card-player.component.ts

---
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-player',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-player.component.html',
+  styleUrl: './card-player.component.css'
+})
+export class CardPlayerComponent {
+
+}
