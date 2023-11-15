c41c1508fb7f5d6a3aefed5485513bc7a32c95da
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 09:03:10 -0300

    Create history-page.component.ts

---
+++ src/app/modules/history/pages/history-page/history-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-history-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './history-page.component.html',
+  styleUrl: './history-page.component.css'
+})
+export class HistoryPageComponent {
+
+}
