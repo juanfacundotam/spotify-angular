7668d17349698f3939426cddb60c91cc9bc342ca
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 09:03:17 -0300

    Create tracks-page.component.ts

---
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tracks-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tracks-page.component.html',
+  styleUrl: './tracks-page.component.css'
+})
+export class TracksPageComponent {
+
+}
