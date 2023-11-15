c395315b0c627bf11dc44ac5134ebf7051624f4a
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 10:05:20 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 export const routes: Routes = [
-{
+  {
     path: 'auth',
-    loadChildren:() => import(`./modules/auth/auth.module`).then(m => m.AuthModule) 
-},
-{
+    loadChildren: () =>
+      import(`./modules/auth/auth.module`).then((m) => m.AuthModule),
+  },
+  {
     path: '',
     component: HomePageComponent,
-    loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule) 
-}
+    loadChildren: () =>
+      import(`./modules/home/home.module`).then((m) => m.HomeModule),
+  },
 ];
