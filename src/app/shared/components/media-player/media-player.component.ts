1b52d523cc2c96c5cc3bdcf2cd3fbd9f6a8fe6f6
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 19:48:07 -0300

    Update media-player.component.ts

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
@@ -9,10 +10,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './media-player.component.css'
 })
 export class MediaPlayerComponent {
- mockCover: any = {
+ mockCover: TrackModel = {
   cover: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F1198260%2Falbum_media_music_icon&psig=AOvVaw2bsZa1XV6r1gNA56k0Imsn&ust=1700142293881000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNDFkN6RxoIDFQAAAAAdAAAAABAE',
   album: 'Yes',
   name: 'Gates',
+  url: "http://localhost/track.mp3",
+  _id: 1,
  }
 
 }
