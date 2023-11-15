9abe2e1b015966cc9f5bf853fe0aa036192bae6c
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 10:05:27 -0300

    Update home.module.ts

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+import { TracksPageComponent } from '@modules/tracks/pages/tracks-page/tracks-page.component';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { SideBarComponent } from '@shared/components/side-bar/side-bar.component
   imports: [
     CommonModule,
     HomeRoutingModule,
-    SideBarComponent
+    SideBarComponent,
+    TracksPageComponent
   ]
 })
 export class HomeModule { }
