8eab9ef2de9f242e4d034ce684d410690047c595
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 11:13:24 -0300

    Update media-player.component.ts

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -9,5 +9,10 @@ import { CommonModule } from '@angular/common';
   styleUrl: './media-player.component.css'
 })
 export class MediaPlayerComponent {
+ mockCover: any = {
+  cover: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F1198260%2Falbum_media_music_icon&psig=AOvVaw2bsZa1XV6r1gNA56k0Imsn&ust=1700142293881000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNDFkN6RxoIDFQAAAAAdAAAAABAE',
+  album: 'Yes',
+  name: 'Gates',
+ }
 
 }
