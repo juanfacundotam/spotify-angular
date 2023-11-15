84caad9c30903ea9165cc94b9be57bc220ce7efb
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 09:03:21 -0300

    Update side-bar.component.ts

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,24 +1,80 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-interface LinkItem {
-  name:string
-}
+// interface LinkItem {
+//   name:string
+// }
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.css'
+  styleUrl: './side-bar.component.css',
 })
 export class SideBarComponent {
-links: Array<LinkItem> = [
-  {
-    name: "Juan",
-  },
-  {
-    name: "Pedro",
-  },
-];
+  //   linksMenu:Array<any> = [
+  //     {name: 'Home',
+  //     icon: 'uil-estate'},
+  //     {name: 'Buscar',
+  //     icon: 'uil-estate'},
+
+  //   ]
+  // links: Array<any> = [
+  //   {
+  //     name: "Juan",
+  //   },
+  //   {
+  //     name: "Pedro",
+  //   },
+  // ];
+  mainMenu: { defaultOptions: Array<any>; accessLink: Array<any> } = {
+    defaultOptions: [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/', 'auth'],
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history'],
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites'],
+        query: { hola: 'mundo' },
+      },
+    ],
+    accessLink: [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square',
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical',
+      },
+    ],
+  };
+
+  customOptions: Array<any> = [
+    {
+      name: 'Mi lista º1',
+      router: ['/'],
+    },
+    {
+      name: 'Mi lista º2',
+      router: ['/'],
+    },
+    {
+      name: 'Mi lista º3',
+      router: ['/'],
+    },
+    {
+      name: 'Mi lista º4',
+      router: ['/'],
+    },
+  ];
 }
