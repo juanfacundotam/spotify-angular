5f6508128275d79c4983712f7768f19596d0a849
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 09:03:02 -0300

    Create favorites-page.component.ts

---
+++ src/app/modules/favorites/pages/favorites-page/favorites-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-favorites-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './favorites-page.component.html',
+  styleUrl: './favorites-page.component.css'
+})
+export class FavoritesPageComponent {
+
+}
