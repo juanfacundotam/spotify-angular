83330950667457bb4c6f110fbf346d924fcc1634
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 19:48:13 -0300

    Update section-generic.component.ts

--- src/app/shared/components/section-generic/section-generic.component.ts
+++ src/app/shared/components/section-generic/section-generic.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TrackModel } from '@core/models/tracks.model';
+import { CardPlayerComponent } from '../card-player/card-player.component';
 
 @Component({
   selector: 'app-section-generic',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardPlayerComponent],
   templateUrl: './section-generic.component.html',
   styleUrl: './section-generic.component.css'
 })
 export class SectionGenericComponent {
-  @Input() title: string = '';
-  @Input() mode: 'small' | 'big' = 'big';
-  @Input() dataTracks: Array<any> = []
+  @Input() title: string = ''
+  @Input() mode: 'small' | 'big' = 'big'
+  @Input() dataTracks: Array<TrackModel> = []
 }
