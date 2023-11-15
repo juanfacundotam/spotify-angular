112f5001f063bccbb5f1bca876534234269021e7
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 10:05:33 -0300

    Update home-page.component.ts

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+import { RouterModule } from '@angular/router';
+import { TracksPageComponent } from '@modules/tracks/pages/tracks-page/tracks-page.component';
+import { MediaPlayerComponent } from '@shared/components/media-player/media-player.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, SideBarComponent],
+  imports: [CommonModule, SideBarComponent, MediaPlayerComponent, RouterModule, TracksPageComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
