acc5b808c89c6a0b935da44868e3ae5a33e39c35
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 09:02:50 -0300

    Create auth-page.component.ts

---
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-auth-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-page.component.html',
+  styleUrl: './auth-page.component.css'
+})
+export class AuthPageComponent {
+
+}
