d106556551bc48ea4e3efbfec58700afb7d66411
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 11:13:13 -0300

    Update tracks-page.component.ts

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SectionGenericComponent } from '@shared/components/section-generic/section-generic.component';
 
 @Component({
   selector: 'app-tracks-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SectionGenericComponent],
   templateUrl: './tracks-page.component.html',
   styleUrl: './tracks-page.component.css'
 })
 export class TracksPageComponent {
+  mockTracksList = [
+    {name: 'BEBE (Oficial)'},
+    {name: 'BEBE (Oficial)'},
+    {name: 'BEBE (Oficial)'},
+  ]
 
 }
