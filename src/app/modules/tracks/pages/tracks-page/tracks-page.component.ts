934e35eb09ec30146b16f9978f22a4010addeb6b
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-15 19:47:58 -0300

    Update tracks-page.component.ts

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionGenericComponent } from '@shared/components/section-generic/section-generic.component';
+import * as dataRaw from '../../../../data/tracks.json';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-tracks-page',
   standalone: true,
   imports: [CommonModule, SectionGenericComponent],
   templateUrl: './tracks-page.component.html',
-  styleUrl: './tracks-page.component.css'
+  styleUrl: './tracks-page.component.css',
 })
-export class TracksPageComponent {
-  mockTracksList = [
-    {name: 'BEBE (Oficial)'},
-    {name: 'BEBE (Oficial)'},
-    {name: 'BEBE (Oficial)'},
-  ]
+export class TracksPageComponent implements OnInit {
+  mockTracksList: Array<TrackModel> = [];
 
+  ngOnInit() {
+    const { data }: any = (dataRaw as any).default;
+    console.log(data);
+    this.mockTracksList = data; // Assuming data is an array of TrackModel
+  }
 }
