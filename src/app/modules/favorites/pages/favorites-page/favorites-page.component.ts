fac8ca32ffba693a0f602f434793741084c58531
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-16 10:52:43 -0300

    Update favorites-page.component.ts

--- src/app/modules/favorites/pages/favorites-page/favorites-page.component.ts
+++ src/app/modules/favorites/pages/favorites-page/favorites-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlayListBodyComponent } from '@shared/components/play-list-body/play-list-body.component';
+import { PlayListHeaderComponent } from '@shared/components/play-list-header/play-list-header.component';
 
 @Component({
   selector: 'app-favorites-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayListBodyComponent, PlayListHeaderComponent],
   templateUrl: './favorites-page.component.html',
   styleUrl: './favorites-page.component.css'
 })
