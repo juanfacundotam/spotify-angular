32b9ffbcc0653df822eddd66fd19e94c147ca66a
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-16 10:53:01 -0300

    Create play-list-body.component.ts

---
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-play-list-body',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './play-list-body.component.html',
+  styleUrl: './play-list-body.component.css'
+})
+export class PlayListBodyComponent {
+
+}
