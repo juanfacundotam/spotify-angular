5da755bff00404d670231c3b90ac6b5f3bf3056e
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-16 10:52:45 -0300

    Update home-routing.module.ts

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -5,21 +5,20 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 const routes: Routes = [
   {
     path: 'tracks',
-    loadChildren: () =>
-      import('@modules/tracks/tracks.module').then((m) => m.TracksModule),
+    loadChildren: () => import('@modules/tracks/tracks.module').then(m => m.TracksModule)
   },
   {
     path: 'favorites',
-    loadChildren: () =>
-      import('@modules/favorites/favorites.module').then(
-        (m) => m.FavoritesModule
-      ),
+    loadChildren: () => import('@modules/favorites/favorites.module').then(m => m.FavoritesModule)
   },
   {
     path: 'history',
-    loadChildren: () =>
-      import('@modules/history/history.module').then((m) => m.HistoryModule),
+    loadChildren: () => import('@modules/history/history.module').then(m => m.HistoryModule)
   },
+  {
+    path: '**',//TODO 404 cuando no existe la ruta
+    redirectTo: '/tracks'
+  }
 ];
 
 @NgModule({
