53e36df1bd5de7b584f00f61b3242fab8df01e20
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-16 10:53:10 -0300

    Create play-list-header.component.ts

---
+++ src/app/shared/components/play-list-header/play-list-header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-play-list-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './play-list-header.component.html',
+  styleUrl: './play-list-header.component.css'
+})
+export class PlayListHeaderComponent {
+
+}
