1ee0db81ed6a909319829063420f37d40def5ef8
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-17 10:40:57 -0300

    Update img-broken.directive.ts

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -5,15 +5,15 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   standalone: true
 })
 export class ImgBrokenDirective {
-  @Input() customImg: string = '' // podriamos hacer esto para pasarle por props al img del html
+  // @Input() customImg: string = '' // podriamos hacer esto para pasarle por props al img del html
 @HostListener('error') handleError(): void {
   const elNative =  this.elHost.nativeElement
   console.log('Esta imagen es inexistente', this.elHost)
- elNative.src = 'assets/noImage.webp'
- elNative.src = this.customImg // y asi pasar distintas img segun sea el caso
+ elNative.src = './assets/noImage.png'
+//  elNative.src = this.customImg // y asi pasar distintas img segun sea el caso
 }
   constructor(private elHost: ElementRef) {
-    console.log(elHost)
+    // console.log(elHost)
    }
 
 }
