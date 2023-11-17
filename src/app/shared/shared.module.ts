d8c71b757b07f760f1134d24a3170b8ea4d9e7f6
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-17 09:41:37 -0300

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,35 +8,29 @@ import { CardPlayerComponent } from './components/card-player/card-player.compon
 import { SectionGenericComponent } from './components/section-generic/section-generic.component';
 import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
 import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
-
-
-
+import { OrderListPipe } from './pipe/order-list.pipe';
+import { ImgBrokenDirective } from './directives/img-broken.directive';
 
 @NgModule({
   declarations: [
     SideBarComponent,
     MediaPlayerComponent,
-
     CardPlayerComponent,
     SectionGenericComponent,
     PlayListHeaderComponent,
     PlayListBodyComponent,
-
-
+   
   ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
+  imports: [CommonModule, RouterModule, OrderListPipe,  ImgBrokenDirective],
   exports: [
     SideBarComponent,
     MediaPlayerComponent,
-
     CardPlayerComponent,
     SectionGenericComponent,
     PlayListHeaderComponent,
     PlayListBodyComponent,
-
-  ]
+    OrderListPipe,
+    ImgBrokenDirective
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
