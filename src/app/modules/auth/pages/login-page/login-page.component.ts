e760218600121e3267d3958c77989a755a0424b4
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-18 10:25:36 -0300

    Update login-page.component.ts

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { AuthService } from '@modules/auth/services/auth.service';
+
 
 @Component({
   selector: 'app-login-page',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+ formLogin: FormGroup = new FormGroup({}) 
+
+ constructor(private authService: AuthService){}
+
+
+ 
+  ngOnInit(): void {
+   this.formLogin = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]),
+   })
+  }
+
+
+  sendLogin():  void {
+    const {email, password} = this.formLogin.value;
+    this.authService.sendCredencials(email, password)
+  }
+
+ 
 
 }
