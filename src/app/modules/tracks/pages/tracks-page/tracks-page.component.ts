4d9ef9d2f97607d0483283c9dd7ae5fd54f703a2
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-23 15:56:21 -0300

    Update tracks-page.component.ts

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,19 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionGenericComponent } from '@shared/components/section-generic/section-generic.component';
 import * as dataRaw from '../../../../data/tracks.json';
 import { TrackModel } from '@core/models/tracks.model';
+import { TrackService } from '@modules/tracks/services/track.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
-  styleUrls: ['./tracks-page.component.css']
+  styleUrls: ['./tracks-page.component.css'],
 })
-export class TracksPageComponent implements OnInit {
-  mockTracksList: Array<TrackModel> = [];
+export class TracksPageComponent implements OnInit, OnDestroy {
+  tracksTrending: Array<TrackModel> = [];
+  tracksRandom: Array<TrackModel> = [];
+  listObservers$: Array<Subscription> = [];
+  constructor(private trackServices: TrackService) {} //generamos la suscripcion del servicio
 
   ngOnInit() {
-    const { data }: any = (dataRaw as any).default;
-    this.mockTracksList = data; // Assuming data is an array of TrackModel
+
+    const observer1$ = this.trackServices.dataTracksTrending$.subscribe(
+      (response) => {
+        this.tracksTrending = response
+        this.tracksRandom = response
+        console.log('Canciones >>', response);
+      }
+    );
+
+    const observer2$ = this.trackServices.dataTracksRandom$.subscribe(
+      (response) => {
+        this.tracksRandom = [...this.tracksRandom, ... response]
+        console.log('Canciones >>', response);
+      }
+    );
+
+    this.listObservers$ = [observer1$, observer2$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u => u.unsubscribe())
   }
 }
