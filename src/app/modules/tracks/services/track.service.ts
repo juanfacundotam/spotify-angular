35460bab9d4b3529b4dd5ae0b728347471cbca8a
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-12-01 10:19:53 -0300

    Update track.service.ts

--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,29 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { Observable, of } from 'rxjs';
-import * as dataRaw from "../../../data/tracks.json"
+import { environment } from 'src/environments/environments';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TrackService {
-dataTracksTrending$: Observable<TrackModel[]> = of([]);
-dataTracksRandom$: Observable<any[]> = of([]);
-  constructor() {
-    const {data}: any = (dataRaw as any).default;
-    this.dataTracksTrending$ = of(data);
-    this.dataTracksRandom$ = new Observable((observer) => {
-      const trackExample: TrackModel = {
-        _id:9,
-        name: "level",
-        album: "2 cuartos",
-        url: "http://",
-        cover: "https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
-      }
-setTimeout(()=>{
-  observer.next([trackExample])
+  private readonly URL = environment.api;
+  constructor(private httpClient: HttpClient) {}
 
-}, 3500)
-    })
-   }
+  getAllTracks$(): Observable {
+    return
+  }
 }
