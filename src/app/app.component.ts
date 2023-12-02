82a17f78df5b99a1079ac912ad4d95f53cf382c6
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-12-02 10:37:22 -0300

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spotify';
 
-//   name: string = "carlos";
-//   auto: AutoTypes = {
-//     marca: "Audi",
-//     year: 2020
-//   }
-  
-//   listCar: Array<AutoTypes> = [
-// {marca: "BW"},
-// {marca: "Toyota", year: 2021}
-//   ]
+
 
 }
-
-  // interface AutoTypes {
-  //   marca: string;
-  //   year?: number;
-  // }
